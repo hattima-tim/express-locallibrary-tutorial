05a304e31d96b79960401a9b1bf9bc1fff8226c4
hattima-tim <irfanridwan20@gmail.com>
2023-05-09 04:43:34 +0000

    add catalog router

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const catalogRouter = require('./routes/catalog');
 const Author = require("./models/author");
 const Book = require("./models/book");
-const Genre = require('./models/genre')
-const mongoose = require("mongoose");
+const Genre = require('./models/genre');
 
 const app = express();
 const mongodb =
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use('/catalog',catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
