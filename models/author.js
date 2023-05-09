8cfef198c0ee3fc8a3812f7977a0c3db396db5ac
hattima-tim <irfanridwan20@gmail.com>
2023-05-09 04:41:49 +0000

    create virtual for showing formatted date

--- models/author.js
+++ models/author.js
@@ -1,3 +1,4 @@
+const {DateTime} = require('luxon');
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -19,6 +20,16 @@ AuthorSchema.virtual("name").get(function () {
 
 AuthorSchema.virtual("url").get(function () {
   return `/catalog/author/${this._id}`;
+});
+
+AuthorSchema.virtual('date_of_birth_formatted').get(function(){
+  return this.date_of_birth?DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED):''
+})
+
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+  return this.date_of_death
+    ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+    : "";
 });
 
 module.exports = mongoose.model("Author", AuthorSchema);
