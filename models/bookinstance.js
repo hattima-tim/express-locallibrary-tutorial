17f972b39209f526100fa1059343770b66b01d8a
hattima-tim <irfanridwan20@gmail.com>
2023-05-17 15:35:17 +0000

    add new virtual property for date

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -23,4 +23,10 @@ BookInstanceSchema.virtual("due_back_formatted").get(function () {
   return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
+BookInstanceSchema.virtual("due_back_ymd").get(function () {
+  return this.due_back
+    ? DateTime.fromJSDate(this.due_back).toFormat("yyyy-MM-dd")
+    : "";
+});
+
 module.exports = mongoose.model("BookInstance", BookInstanceSchema);
